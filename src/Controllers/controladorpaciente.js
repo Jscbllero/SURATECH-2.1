63363cc319e5e5fc0b9d97f95f0ae56ec2589d9a
johjan florez <jflorez1@cesde.net>
2024-10-23 16:13:21 -0500

    se crea copiaSuratech2.1

---
+++ src/Controllers/controladorpaciente.js
@@ -0,0 +1,13 @@
+//capturar los datos de un formulario
+/* por cada input, select, textarea del formulario se crea una variable*/
+
+let nombrePaciente=document.getElementById("nombrepaciente")
+let correoPaciente=document.getElementById("correopaciente")
+let telefonoPaciente=document.getElementById("telefonopaciente")
+let fechaNacimientoPaciente=document.getElementById("nacimientopaciente")
+let ciudadPaciente=document.getElementById("ciudadpaciente")
+let ipsPaciente=document.getElementById("ipspaciente")
+let grupoIngresoPaciente=document.getElementById("ingresospaciente")
+let fechaAfiliacionPaciente=document.getElementById("fechapaciente")
+
+//2. por cada variable debo utilizar DOM para asociar el html con js
